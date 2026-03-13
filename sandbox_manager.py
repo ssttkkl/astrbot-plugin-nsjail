d43cc347f859c3d43a4cca082ab9c84156cd1175
ssttkkl <huang.wen.long@hotmail.com>
2026-03-13 23:14:47 +0800

    fix: 添加环境变量隔离

--- sandbox_manager.py
+++ sandbox_manager.py
@@ -149,6 +149,7 @@ class SandboxManager:
                 nsjail_cmd.extend(["--cgroup_cpu_ms_per_sec", str(cpu_ms_per_sec)])
         
         nsjail_cmd.extend([
+            "--clear_env",
             "--env", "PATH=/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/sbin:/sbin",
             "--env", "UV_CACHE_DIR=/sandbox-cache/uv",
             "--env", "HOME=/workspace",
